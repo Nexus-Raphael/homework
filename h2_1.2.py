4e300a7507ccf4fbb4b03171d0249ceae8d53ad0
LUO KUN <raphael.kun.luo@outlook.com>
2024-11-07 13:37:03 +0800

    feat(h2): finished homework 2
    To be familiar with it while practicing to enhance that thing.

---
+++ h2_1.2.py
@@ -0,0 +1,11 @@
+# #就我目前所掌握的,只能查找出某些常用的邮箱地址,见谅.
+
+import re
+r=r'(^[\w-]+(\.[\w-]+)*@(qq|outlook|163|126|gmail|sina|hotmail|139|sohu|icloud|smail)(\.[\w-]+)+$)'
+adress='alphal.it.is.my.turn@outlook.com'
+print(re.match(r,adress))
+
+print('===============================================================================================================')
+text=input()
+pn=r'\d{3}-\d{8}'
+print(re.findall(pn,text))
