f65d4d5bac7c74763521ca2c3def53daa9da3e1c
LUO KUN <raphael.kun.luo@outlook.com>
2024-11-07 14:31:12 +0800

    Delete beginning/h2_1.3.py

--- beginning/h2_1.3.py
+++
@@ -1,8 +0,0 @@
-print("My name is %s and my age is %d and my profession is %s"%('lk',18,'chifan,shuijiao,dadoudou'))
-print('====================================================================================================')
-s=input()
-com=s.split(' ')
-news=[lie[::-1] for lie in com]
-outcome=' '.join(news)
-print(outcome)
-
